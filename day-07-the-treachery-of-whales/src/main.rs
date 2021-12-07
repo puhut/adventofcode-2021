1d2ec6d9a621f96933824fd97ad1aef15dc6760e
Heejae Park <hpark@fandango.com>
2021-12-07 10:12:36 -0800

    with bonus

--- day-07-the-treachery-of-whales/src/main.rs
+++ day-07-the-treachery-of-whales/src/main.rs
@@ -23,7 +23,18 @@ fn main() {
                     }
                     position_sum_board.insert(*p1, sum);
                 }
-
+                /* bonus
+                let position_of_crabs : Vec<i32> = line_of_crabs.split(",").filter_map(|x| x.parse().ok()).collect();
+                for p1 in 0..1856 {
+                    let mut sum = 0;
+                    for p2 in &position_of_crabs {
+                        let v = i32::abs(p1-p2);
+                        let result = v*(v+1)/2;
+                        sum += result;
+                    }
+                    position_sum_board.insert(p1, sum);
+                }
+                */
             }
         }
     }
